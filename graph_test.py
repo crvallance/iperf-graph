0d0100cb3c8f35cbc76bb59287fce69a9203a5d6
crvallance <crvallance@gmail.com>
2020-02-22 18:10:46 -0600

    Adding in some basic parsing to allow more dynamic key labels for graph

--- graph_test.py
+++ graph_test.py
@@ -3,6 +3,7 @@ from matplotlib import style
 import json
 import pathlib
 import argparse
+import re
 
 parser = argparse.ArgumentParser(description='Somestuff!')
 parser.add_argument("--f", nargs='*', help='Pass in the files to be graphed together')
@@ -11,7 +12,27 @@ sum_stream = parser.add_mutually_exclusive_group(required=False)
 sum_stream.add_argument("--sum", action='store_true', help='Used to only graph sums')
 sum_stream.add_argument("--stream", action='store_true', help='Used to graph each stream')
 parser.add_argument("--title", help='Manually give the graph a title')
+key_parse = parser.add_argument_group()
+key_parse.add_argument("--kd", help='Designate a delimeter for filename to key parsing')
+key_parse.add_argument("--km", help='"Mask" for new desired output of key name.  Use numbers to designate order, omit fields, and additional text as needed.\nExample: "1 2 4 small 3" would produce "cat dog snake small fish" from a file called "cat_dog_fish_snake.json"')
 
+def key_parse(args, coded_name):
+    pattern = re.compile(r'([0-9])')
+    delim = args.kd
+    mask = args.km
+    result = mask
+    try:
+        elements = coded_name.split(delim)
+    except:
+        print('Something wrong')
+
+    for m in re.finditer(pattern, mask):
+        mask_pos = m.group(1)
+        p = re.compile(str(mask_pos))
+        file_pos = int(m.group(1))-1
+        result = p.sub(elements[file_pos], result, count=1)
+        #print(result)
+    return(result)
 
 def in_progress(args):
     # style
@@ -21,6 +42,10 @@ def in_progress(args):
         x = []
         y = []
         p = pathlib.Path(input_file)
+        if args.kd and args.km:
+            label = key_parse(args, p.stem)
+        else:
+            label = p.stem
         f = open(input_file)
         try:
             data = json.load(f)
@@ -37,7 +62,7 @@ def in_progress(args):
         # create a color palette
         palette = plt.get_cmap('tab20b')
         plt.subplots_adjust(right=0.7)
-        plt.plot(x, y, color=palette(cp), label=p.stem, linewidth=3)
+        plt.plot(x, y, color=palette(cp), label=label, linewidth=3)
         # plt.plot(x1,y1,'c',label='line two',linewidth=5)
         cp += 1
     if args.title:
