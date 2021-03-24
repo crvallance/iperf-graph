58917d43df79a1b11fd2ec3378e992784e1053b5
crvallance <crvallance@protonmail.com>
2021-03-23 21:21:43 -0500

    better error handling

--- graph_test.py
+++ graph_test.py
@@ -4,6 +4,7 @@ import json
 import pathlib
 import argparse
 import re
+import sys
 
 parser = argparse.ArgumentParser(description='Somestuff!')
 parser.add_argument("--f", nargs='*', help='Pass in the files to be graphed together')
@@ -15,6 +16,7 @@ parser.add_argument("--title", help='Manually give the graph a title')
 key_parse = parser.add_argument_group()
 key_parse.add_argument("--kd", help='Designate a delimeter for filename to key parsing')
 key_parse.add_argument("--km", help='"Mask" for new desired output of key name.  Use numbers to designate order, omit fields, and additional text as needed.\nExample: "1 2 4 small 3" would produce "cat dog snake small fish" from a file called "cat_dog_fish_snake.json"')
+parser.add_argument("--noshow", help='Save to file without showing output')
 
 def key_parse(args, coded_name):
     pattern = re.compile(r'([0-9])')
@@ -49,8 +51,10 @@ def in_progress(args):
         f = open(input_file)
         try:
             data = json.load(f)
-        except:
-            print(input_file)
+        except json.decoder.JSONDecodeError as e:
+            print(e)
+            sys.exit(input_file + ' has a JSON error')
+            # print(e)
             # raise
         # print(data['end'].keys())
         for item in data['intervals']:
@@ -73,28 +77,11 @@ def in_progress(args):
     plt.xlabel('Time (Seconds)')
     plt.grid(True, color='k')
     plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
-    plt.show()
-
-
-'''
-#use('ggplot')
-x=[5,8,10]
-y=[12,16,6]
-x1=[6,9,11]
-y1=[6,15,7]
-plt.plot(x,y,'g',label='line one',linewidth=5)
-plt.plot(x1,y1,'c',label='line two',linewidth=5)
-plt.title('iPerf Data')
-plt.ylabel('Bytes/Second')
-plt.xlabel('Time (Seconds)')
-plt.grid(True,color='K')
-plt.legend()
-plt.show()
-'''
-
-# https://www.probytes.net/blog/plotting-graphs-in-python/
-# https://python-graph-gallery.com/124-spaghetti-plot/
-
+    if args.noshow:
+        print(args.fi)
+        plt.savefig('foo.png')
+    else:
+        plt.show()
 
 def main():
     args = parser.parse_args()
