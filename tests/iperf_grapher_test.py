11d927ee5bed5afcbe93f95b46e6281b58a6f417
crvallance <crvallance@protonmail.com>
2022-01-31 20:57:49 -0600

    thanks Scott

--- tests/iperf_grapher_test.py
+++ tests/iperf_grapher_test.py
@@ -1,7 +1,16 @@
 import pathlib
+import hashlib
 
 import pytest
 from iperf_grapher import ArgsShim, grapher
+
+
+def hasher(filename):
+    with open(filename, "rb") as f:
+        file_hash = hashlib.blake2b()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+    return(file_hash.hexdigest())
 
 
 @pytest.fixture
@@ -17,4 +26,7 @@ def test_grapher(arg_setup):
     grapher(args)
     outfile = pathlib.Path(f'./examples/{example_file}.png')
     assert outfile.exists()
+    blake2b_hash = '659b6f5fba3bf14fac8fbe3b8dd63e0df554435d888f6102df70d6123253d8bb3b817b0dd2b432f2c745c8c820a3288a1349bc3947d3d7a3766a8b597698a58b'
+    calc_hash = hasher(outfile)
+    assert calc_hash == blake2b_hash
     outfile.unlink()
